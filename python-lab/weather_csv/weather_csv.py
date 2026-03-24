1d41e11c4199083051976c378a569a697fbcdc7b
seoyoung <seoyoung.project@gmail.com>
2026-03-24 23:52:26 +0900

    excluding csv

--- python-lab/weather_csv/weather_csv.py
+++ python-lab/weather_csv/weather_csv.py
@@ -7,4 +7,6 @@ with open("weather_data.csv") as data_file:
     data = csv.reader(data_file)
     temperatures = []
     for row in data:
-        row([1])
+        if row[1] != "temp":
+            temperatures.append(row[1])
+    print(temperatures)
